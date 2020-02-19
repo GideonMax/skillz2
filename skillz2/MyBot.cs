4c8edc9adf25635a17b9a4910c7f849df29ecb89
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-02-19 16:28:58 +0200

    Add project files.

---
+++ skillz2/MyBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PenguinGame;
+using System.Linq;
+
+
+namespace MyBot
+{
+    class MyBot :ISkillzBot
+    {
+        public void DoTurn(Game game)
+        {
+            
+        }
+    }
+    public static class utils
+    {
+
+    }
+}
